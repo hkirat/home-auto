8284c08b11513c9e35c69aded18e6547c5f1167d
harkirat <harkirat96@gmail.com>
2017-03-18 07:57:05 +0530

    Changed pin

---
+++ a.py
@@ -0,0 +1,11 @@
+import sys
+
+def main(args):
+    print args[1] 
+    print args[2] 
+    if args[2] == 'on':
+        print "totototo"
+
+
+if __name__ == '__main__':
+    main(sys.argv)
--- on.py
+++ on.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import sys
+from gpiozero import LED, Button
 
 GPIO.setmode(GPIO.BCM)  # set board mode to Broadcom
 
@@ -7,18 +8,15 @@ def main(args):
     if len(args) < 3:
         sys.exit("Usage: python awesome.py <led> <on/off>")
 
-    pin = args[1]
+    pin = int(args[1])
     on = 0
 
+    led1 = LED(pin)
     if args[2] == 'on':
-        on = 1
+        led1.on();
 
     if args[2] == 'off':
-        on = 0
-
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, on)
-
+        led1.off();
 
 if __name__ == '__main__':
     main(sys.argv)
